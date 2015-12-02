449d8ee5d00138cd95f707faa3454e8d8bafcb71
Damir Mustafin <frenchthebread@gmail.com>
2015-12-02 16:34:32 +0900

    commented

--- server/elastic-rest.js
+++ server/elastic-rest.js
@@ -6,23 +6,27 @@
  * @param {object} query: query to be used by elasticsearch to return data
  * @param {array} fields: fields to be returned within provided query
  */
-ElasticRest = function (index, type, size, query, fields) {
+ElasticRest = function (config, options) {
 
-    // initial values
-    this.index  = "";
-    this.type   = "";
-    this.size   = 0;
-    this.query  = {
-        match_all: {}
-    };
-    this.fields = [];
+    console.log(config);
+    console.log(options);
 
-    // assigning provided values to local variables
-    this.index  = index;
-    this.type   = type;
-    this.size   = size;
-    this.query  = query;
-    this.fields = fields;
+    //// initial values
+    //this.index  = "";
+    //this.type   = "";
+    //this.size   = 0;
+    //this.query  = {
+    //    match_all: {}
+    //};
+    //this.fields = [];
+    //
+    //// assigning provided values to local variables
+    //this.index  = index;
+    //this.type   = type;
+    //this.size   = size;
+    //this.query  = query;
+    //this.fields = fields;
+    //
 
     ElasticSearch = Meteor.npmRequire('elasticsearch');
 
@@ -34,28 +38,30 @@ ElasticRest = function (index, type, size, query, fields) {
     // make it fiber aware
     EsClient = Async.wrap(EsClientSource, ['index', 'search']);
 
-    /**
-     *  Search method
-     */
-    this.doSearch = function () {
+    ///**
+    // *  Search method
+    // */
+    //this.doSearch = function () {
+    //
+    //    if(!this.query  || this.query   ==  {}  || this.query   ==  "") this.query  = { match_all: {} };
+    //
+    //    var body = {
+    //        query: this.query,
+    //        size: this.size
+    //    };
+    //
+    //    if(!this.fields || this.fields  ==  []  || this.fields  ==  "") { } else { body.fields = this.fields; }
+    //
+    //    var searchData = EsClient.search({
+    //        index   : this.index,
+    //        type    : this.type,
+    //        body    : body
+    //    });
+    //
+    //
+    //    return searchData;
+    //};
 
-        if(!this.query  || this.query   ==  {}  || this.query   ==  "") this.query  = { match_all: {} };
-
-        var body = {
-            query: this.query,
-            size: this.size
-        };
-
-        if(!this.fields || this.fields  ==  []  || this.fields  ==  "") { } else { body.fields = this.fields; }
-
-        var searchData = EsClient.search({
-            index   : this.index,
-            type    : this.type,
-            body    : body
-        });
-
-
-        return searchData;
-    };
+    return EsClient;
 
 };
