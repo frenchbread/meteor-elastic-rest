07c17649d50406272eaab03255dd9b7ac4b2a281
Damir Mustafin <frenchthebread@gmail.com>
2015-12-02 17:47:23 +0900

    documentation & comments

--- server/elastic-rest.js
+++ server/elastic-rest.js
@@ -1,41 +1,45 @@
 /**
- * Represents Query class
- * @param {string} index: index provided within the query
- * @param {string} type: type of records ro be returned
- * @param {integer} size: limit of records to be returned
- * @param {object} query: query to be used by elasticsearch to return data
- * @param {array} fields: fields to be returned within provided query
+ * ElasticRest
+ * Wrapper around elasticsearch NPM package.
+ * @param {object} config: Configuration object.
+ * @param {string} config.host: Elastic search instance URL. (example: http://elasticsearch.example.com:14002)
+ *
+ * @param {object}  options: Options object.
+ * @param {string}  options.index: Search index.
+ * @param {string}  options.type: Type of records ro be returned.
+ * @param {int}     options.size: Amount of records to be returned.
+ * @param {object}  options.query: Query options.
+ * @param {array}   options.fields: Return specific fields
  */
 ElasticRest = function (config, options) {
 
     // Get reference to 'this'
     var self = this;
 
-    config = {
-        esHost: Meteor.settings.host
-    };
+    //config = {
+    //    host: "hostname"
+    //};
 
-    options = {
-        index: 'api-umbrella-logs-v1-2015-12',
-        type: 'log',
-        size: 10,
-        query: {
-            match_all: {}
-        },
-        fields: [
-            'request_at',
-            'request_ip_country',
-            'request_ip',
-            'response_time',
-            'request_path',
-            'request_ip_location.lon',
-            'request_ip_location.lat'
-        ]
-    };
+    //options = {
+    //    index: 'api-umbrella-logs-v1-2015-12',
+    //    type: 'log',
+    //    size: 10,
+    //    query: {
+    //        match_all: {}
+    //    },
+    //    fields: [
+    //        'request_at',
+    //        'request_ip_country',
+    //        'request_ip',
+    //        'response_time',
+    //        'request_path',
+    //        'request_ip_location.lon',
+    //        'request_ip_location.lat'
+    //    ]
+    //};
 
-    // TODO: change to config.host
     // Initialise host
-    self._host = Meteor.settings.host;
+    self._host = config.host;
 
     // Initialise options
     self._index     = options.index;
@@ -44,20 +48,16 @@ ElasticRest = function (config, options) {
     self._query     = options.query;
     self._fields    = options.fields;
 
+
     // Include NPM elasticsearch package (https://www.npmjs.com/package/elasticsearch)
     var ElasticSearch = Meteor.npmRequire('elasticsearch');
 
     // Create the client
-    var EsClientSource = new ElasticSearch.Client({
-        host: Meteor.settings.elasticsearch.host
-    });
+    var EsClientSource = new ElasticSearch.Client({ host: self._host });
 
     // Make it fiber aware
     var EsClient = Async.wrap(EsClientSource, ['index', 'search']);
 
-    /**
-     *  Search method
-     */
     self.doSearch = function () {
 
         var results = EsClient.search({
@@ -66,6 +66,7 @@ ElasticRest = function (config, options) {
             body    : {
                 query: self._query,
                 size: self._size
+                fields: self._fields
             }
         });
 
