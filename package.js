56a037b607c786d9df66e3a3fe7f719e5a7004db
Damir Mustafin <frenchthebread@gmail.com>
2015-12-02 17:07:21 +0900

    version updates

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'frenchbread:elastic-rest',
-    version: '0.2.3',
+    version: '0.2.5',
     // Brief, one-line summary of the package.
     summary: "Wrapper for Elastic REST API.",
     // URL to the Git repository containing the source code for this package.
