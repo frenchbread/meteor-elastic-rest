24c62cbfaf6c34d218dec787c6b8deb28b7f948e
Damir Mustafin <frenchthebread@gmail.com>
2016-05-25 16:26:51 +0300

    Revert "version ++"
    This reverts commit 0013019f3ea1da110b38e25ec0ef13b78d233177.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'frenchbread:elastic-rest',
-    version: '2.0.1',
+    version: '2.0.0',
     // Brief, one-line summary of the package.
     summary: "Wrapper for Elastic REST API.",
     // URL to the Git repository containing the source code for this package.
