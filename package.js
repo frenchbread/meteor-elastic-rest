f3f8d93c9da295ddf053e7a9bd7cb867908d09da
Damir Mustafin <frenchthebread@gmail.com>
2016-05-25 15:20:46 +0300

    add ecmascript

--- package.js
+++ package.js
@@ -12,6 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
     api.versionsFrom('1.2.1');
+    api.use(['ecmascript'])
     api.addFiles('server/elastic-rest.js');
     api.export("ElasticRest", ['server']);
 });
